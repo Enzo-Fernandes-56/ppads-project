105ccb7e67331867b11fd45d03baf1f7674f6aa5
Enzo-Fernandes-56 <enzo56goveia@gmail.com>
2025-05-30 23:04:27 -0300

    Update main (1).py
    Entrega final do código do Projeto de Praticas Profissionais em Análise e Desenvolvimento de Sistemas

--- main (1).py
+++ main (1).py
@@ -1,191 +1,127 @@
-# Abre o chrome
 import win32com.client as win32
 import time
-import requests
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from datetime import date
-from datetime import datetime
+from datetime import date, datetime
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
+# Criacao da janela oculta para entrada do email e senha do aluno
+root = tk.Tk()
+root.withdraw()
 
+# Coleta dos dados de login usando caixas de entrada
+email_login = simpledialog.askstring("Login", "Digite seu e-mail Mackenzie:")
+senha_login = simpledialog.askstring("Senha", "Digite sua senha:", show='*')
+
+# Inicializa o Chrome com WebDriver Manager para facilitar a compatibilidade
 servico = Service(ChromeDriverManager().install())
-
 navegador = webdriver.Chrome(service=servico)
 navegador.maximize_window()
+navegador.get("https://ava.mackenzie.br/")  # Abre o site do Moodle Mackenzie
 
+# Funcoes auxiliares
 
 def existe_elemento(elemento):
-    if len(elemento) == 0:
-        return False
-    else:
-        return True
+    return len(elemento) > 0
 
-
-def num_mes(x):
-    months = {
-        'jan': 1,
-        'fev': 2,
-        'mar': 3,
-        'abr': 4,
-        'mai': 5,
-        'jun': 6,
-        'jul': 7,
-        'ago': 8,
-        'set': 9,
-        'out': 10,
-        'nov': 11,
-        'dez': 12
-    }
-    a = x.strip()[:3].lower()
-    ez = months[a]
-    return ez
-
+def num_mes(x):  # Converte nome do mês para número
+    months = {'jan': 1, 'fev': 2, 'mar': 3, 'abr': 4, 'mai': 5, 'jun': 6,
+              'jul': 7, 'ago': 8, 'set': 9, 'out': 10, 'nov': 11, 'dez': 12}
+    return months[x.strip()[:3].lower()]
 
 data_atual = date.today()
+time.sleep(4)  # Espera para a página carregar
 
-# Login e Senha do aluno
-# Login
-navegador.find_element(
-    "xpath", '//*[@id="i0116"]').send_keys("10415482@mackenzista.com.br")
+# Realiza login no Moodle usando os dados do aluno
+navegador.find_element("xpath", '//*[@id="i0116"]').send_keys(email_login)
 navegador.find_element("xpath", '//*[@id="idSIButton9"]').click()
+time.sleep(2)
+navegador.find_element("xpath", '//*[@id="i0118"]').send_keys(senha_login)
+navegador.find_element("xpath", '//*[@id="idSIButton9"]').click()
+time.sleep(2)
+navegador.find_element("xpath", '//*[@id="idSIButton9"]').click()
+time.sleep(2)
 
-# Senha
-navegador.find_element("xpath", '//*[@id="i0118"]').send_keys("Nutela22+")
-time.sleep(2)
-navegador.find_element("xpath", '//*[@id="idSIButton9"]').click()
-time.sleep(2)
-# Botão de continuar conectado
-navegador.find_element("xpath", '//*[@id="idSIButton9"]').click()
-time.sleep(2)
-# Botão do Calendário
-navegador.find_element(
-    "xpath", '//*[@id="global_nav_calendar_link"]/div[1]').click()
+# Acessa a página de calendário para encontrar atividades
+navegador.find_element("xpath", '//*[@id="global_nav_calendar_link"]/div[1]').click()
 time.sleep(3)
 
-# Le Calendario
-lista_tarefa = []
+# Busca pelos elementos de tarefas e inicializa listas para os dados
 tarefas = navegador.find_elements("class name", "fc-end")
-# Quantidade de Eventos
-contador = 0
+lista_materias, lista_nome_atividades, lista_data = [], [], []
 
-# lista de materias
-lista_materias = []
-
-# lista do nome da atividade
-lista_nome_atividades = []
-# Quantidade Tarefas Concluídas
-qtd_concluidas = 0
-# lista data atividades
-lista_data = []
-# try:
+# Para cada tarefa encontrada, extrai os dados (matéria, nome, data)
 for i in tarefas:
-    i.click()
-    elements = navegador.find_elements("class name", 'view_event_link')
-    verifica1 = existe_elemento(elements)
-    elements = navegador.find_elements(
-        "xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/time')
-    verifica_data1 = existe_elemento(elements)
-    # elements = navegador.find_elements("xpath", '//*[@id="calendar-app"]/div[4]/div/div/table/tbody/tr/td/div/div/div[3]/div[2]/table/tbody/tr[2]/td[3]/a/div/span[3]')
-    # verifica_tarefas_concluidas = existe_elemento(elements)
-    # if verifica_tarefas_concluidas:
-    #     print("Achamos uma tarefa concluída")
-    #     qtd_concluidas += 1
-    if verifica1:
-        texto = navegador.find_element(
-            "xpath", ' //*[@id="event-details-trap-focus"]/div[2]/table/tbody/tr[1]/td/a').text
-        texto1 = texto.split("-")
-        print(texto1[0])
-        lista_materias.append(texto1[0])
-        print(navegador.find_element("class name", 'view_event_link').text)
-        nom_atividade = navegador.find_element(
-            "class name", 'view_event_link').text
-        lista_nome_atividades.append(nom_atividade)
-        if verifica_data1:
-            print(navegador.find_element(
-                "xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/time').text)
-            data = navegador.find_element(
-                "xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/time').text
-            lista_data.append(data)
-        else:
-            print(navegador.find_element(
-                "xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/span/time[1]').text)
-            data = navegador.find_element(
-                "xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/span/time[1]').text
-            lista_data.append(data)
-    elements = navegador.find_elements(
-        By.CSS_SELECTOR, '.details_title .title')
-    verifica2 = existe_elemento(elements)
-    if verifica2:
-        print(navegador.find_element("class name", 'view_event_link').text)
-    navegador.find_element(
-        "xpath", '//*[@id="event-details-trap-focus"]/a').click()
-    contador += 1
+    try:
+        i.click()
+        elements = navegador.find_elements("class name", 'view_event_link')
+        verifica1 = existe_elemento(elements)
+        elements = navegador.find_elements("xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/time')
+        verifica_data1 = existe_elemento(elements)
 
-    # Formatação Data
-num = 0
+        if verifica1:
+            texto = navegador.find_element("xpath", ' //*[@id="event-details-trap-focus"]/div[2]/table/tbody/tr[1]/td/a').text
+            texto1 = texto.split("-")
+            lista_materias.append(texto1[0])
+            nom_atividade = navegador.find_element("class name", 'view_event_link').text
+            lista_nome_atividades.append(nom_atividade)
+            if verifica_data1:
+                data = navegador.find_element("xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/time').text
+            else:
+                data = navegador.find_element("xpath", '//*[@id="event-details-trap-focus"]/div[2]/div[1]/span/time[1]').text
+            lista_data.append(data)
+
+        navegador.find_element("xpath", '//*[@id="event-details-trap-focus"]/a').click()
+    except:
+        continue
+
+# Converte as datas extraídas para o formato correto (tipo date)
 lista_data2 = []
 for i in lista_data:
     if "," in i:
-        sem_virgula = i.split(",")
-        data_separada = sem_virgula[0].split(" ")
-        lista_data[num] = data_separada[0] + " " + data_separada[1]
-        data_completa = str(data_atual.year) + "-" + \
-            str(num_mes(data_separada[1])) + "-" + str(data_separada[0])
-        data_final = datetime.strptime(data_completa, "%Y-%m-%d").date()
-        lista_data2.append(data_final)
+        data_separada = i.split(",")[0].split(" ")
     else:
         data_separada = i.split(" ")
-        # print(data_separada[0] + " " + data_separada[1])
-        lista_data[num] = data_separada[0] + " " + data_separada[1]
-        data_completa = str(data_atual.year) + "-" + \
-            str(num_mes(data_separada[1])) + "-" + str(data_separada[0])
-        data_final = datetime.strptime(data_completa, "%Y-%m-%d").date()
-        lista_data2.append(data_final)
-    num += 1
+    data_completa = f"{data_atual.year}-{num_mes(data_separada[1])}-{data_separada[0]}"
+    data_final = datetime.strptime(data_completa, "%Y-%m-%d").date()
+    lista_data2.append(data_final)
 
-    contador = 0
-corpo_email = ""  # Variável que vai guardar o texto do e-mail
-
-for i in lista_data2:
-    if i >= data_atual:
-        corpo_email += f"Matéria: {lista_materias[contador]}\n"
-        corpo_email += f"Atividade: {lista_nome_atividades[contador]}\n"
-        corpo_email += f"Data: {lista_data2[contador]}\n"
-        corpo_email += "-" * 30 + "\n"  # linha separadora
-    else:
-        corpo_email += f"A tarefa da matéria {lista_materias[contador]} passou do prazo.\n"
-        corpo_email += "-" * 30 + "\n"
-    contador += 1
-
-    import win32com
-
-
-outlook = win32com.client.Dispatch("Outlook.Application")
-
-
-email = outlook.CreateItem(0)
-
-materia = "Tópicos de Engenharia de Software"
-data = "20-04"
-
-# Email info
-email.To = "<enzo56goveia@gmail.com>"
-email.Subject = " Lembrete de Entrega da Atividade de {materia} ".format(
-    materia=materia)
-email.HTMLBody = f""" 
-<p>Olá Aluno!  Não esqueça que o prazo para envio da tarefa é até {data}.</p>
-{corpo_email}
-<p>Faça suas ativiades em dia para não ficar de recuperação</p>
-
-<p>Bons estudos!</p>
-<p>Taskbot :)</p>
+# Cria uma tabela HTML com os dados das atividades
+corpo_email = """
+<table border="1" cellpadding="8" cellspacing="0" style="border-collapse: collapse; width: 100%; font-family: Arial, sans-serif; font-size: 14px;">
+    <thead style="background-color: #f2f2f2;">
+        <tr>
+            <th>#</th><th>Matéria</th><th>Atividade</th><th>Data</th><th>Status</th>
+        </tr>
+    </thead><tbody>
 """
 
+# Preenche a tabela com os dados de cada atividade
+for i in range(len(lista_data2)):
+    materia = lista_materias[i]
+    atividade = lista_nome_atividades[i]
+    data_entrega = lista_data2[i].strftime("%d/%m/%Y")
+    status = "<span style='color: red;'>⚠ Atrasada</span>" if lista_data2[i] < data_atual else "<span style='color: green;'>✅ Dentro do prazo</span>"
+    corpo_email += f"<tr><td>{i+1}</td><td>{materia}</td><td>{atividade}</td><td>{data_entrega}</td><td>{status}</td></tr>"
 
+corpo_email += "</tbody></table>"
+
+# Monta e envia o e-mail pelo Outlook com o resumo das pendências
+outlook = win32.Dispatch("Outlook.Application")
+email = outlook.CreateItem(0)
+email.To = email_login
+hoje = datetime.now().strftime("%d/%m/%Y")
+email.Subject = f"Relatório Geral de Tarefas - {hoje}"
+email.HTMLBody = f""" 
+<p style="font-family: Arial, sans-serif; font-size: 15px;">Olá Aluno! Aqui está o <strong>resumo de tarefas encontradas</strong>:</p>
+{corpo_email}
+<p style="font-family: Arial, sans-serif; font-size: 15px;">Faça suas atividades em dia para não ficar de recuperação.</p>
+<p style="font-family: Arial, sans-serif; font-size: 15px;">Bons estudos!<br><strong>Taskbot :)</strong></p>
+"""
 email.Send()
-print(email.To)
 
-print("==> Branch iteracao2 ativa: teste de diferenciação")
+# Mensagem final informando que o processo foi concluído
+messagebox.showinfo("Finalizado", "Relatório enviado com sucesso!")
